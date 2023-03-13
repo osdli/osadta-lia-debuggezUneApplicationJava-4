c63193b8329ed758c1ff36b63a2cc40848580afd
Lia Osadta <q4060zns92g1-student@anonymous-codio.com>
2023-03-13 13:55:51 +0000

    refactor analytics counter

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,40 +3,61 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	
-	private static int rashCount = 0;		
-	private static int pupilCount = 0;		
-	
-	public static void main(String args[]) throws Exception {
-		
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-		int i = 0;	
-		int headCount = 0;	
-		while (line != null) {
-			i++;	
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+    ISymptomReader readerInstance;
+    ISymptomWriter writerInstance;
+
+    
+    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer){
+        this.readerInstance = reader;
+        this.writerInstance = writer;
+    }
+
+    /**
+     * Returns a list of symptoms read from the configured ISymptomReader.
+     */
+    public List<String> getSymptoms() { 
+        return readerInstance.getSymptoms();
+    }
+
+    /**
+     * Counts the occurrences of each symptom in the given list of symptoms.
+     */
+    public Map<String, Integer> countSymptoms (List<String> symptoms) {
+        Map<String, Integer> symptomCounts = new HashMap<>();
+        for (String symptom : symptoms) {
+            if (symptomCounts.containsKey(symptom)) {
+                symptomCounts.put(symptom, symptomCounts.get(symptom) + 1);
+            } else {
+                symptomCounts.put(symptom, 1);
+            }
+        }
+        return symptomCounts;
+    }
+
+    /**
+     * Sorts the given map of symptoms and their counts by the symptom names in alphabetical order.
+     */
+    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+        TreeMap<String, Integer> sortedSymptoms = new TreeMap<>(symptoms);
+        return sortedSymptoms;
+    }
+
+    /**
+     * Writes the given map of symptom names and their counts to the configured ISymptomWriter.
+     */
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        writerInstance.writeSymptoms(symptoms);
+    }
+
+    public static void main(String args[]) throws Exception {
+
+    }
 }
